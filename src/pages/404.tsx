4bac5bd3c745b6824090a37563fc6cf37b3a3bca
= <=>
2021-05-17 08:48:50 -0700

    adds custom 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,12 +2,20 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { HomeSection } from "../components/styled"
+import tw from "twin.macro"
+import "twin.macro"
 
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <HomeSection tw="flex justify-center">
+      <div tw="text-center">
+        <h1 tw="text-5xl font-tmono">404!</h1>
+        <p>Oh no! This page doesn't exist.</p>
+        <p>Check the URL that got you here.</p>
+      </div>
+    </HomeSection>
   </Layout>
 )
 
